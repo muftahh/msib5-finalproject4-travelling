e0f49cbfa84a22471b8e0d3777ec8db9d5f4ea4b
muftahh <muftahnaser54321@gmail.com>
2023-12-06 20:57:07 +0700

    tambah gambar dan warna (yang tadi format namanya salah)

--- app/src/main/java/com/hacktiv8/travelling3/SplashScreenActivity.java
+++ app/src/main/java/com/hacktiv8/travelling3/SplashScreenActivity.java
@@ -2,13 +2,25 @@ package com.hacktiv8.travelling3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
+@SuppressLint("CustomSplashScreen")
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(SplashScreenActivity.this, WelcomeActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
+
     }
 }
