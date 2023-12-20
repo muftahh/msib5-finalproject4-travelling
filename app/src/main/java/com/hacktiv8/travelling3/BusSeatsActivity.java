13c3612d4c4b2f1d1e18ef8a160312d058d84b4e
Ahmad Satriadi <ahmadsatriadimm93@gmail.com>
2023-12-20 09:03:03 +0800

    pilih kursi

--- app/src/main/java/com/hacktiv8/travelling3/BusSeatsActivity.java
+++ app/src/main/java/com/hacktiv8/travelling3/BusSeatsActivity.java
@@ -5,14 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class BusSeatsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    private TextView ptName, tanggalWaktu, facility;
+public class BusSeatsActivity extends AppCompatActivity  {
+
+    private TextView ptName, tanggalWaktu, facility, priceTv;
+    private List<ImageButton> imageButtons = new ArrayList<>();
+    private int priceFinal = 0;
+    private int priceBus;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +32,7 @@ public class BusSeatsActivity extends AppCompatActivity {
         ptName = findViewById(R.id.pt_name);
         tanggalWaktu = findViewById(R.id.tanggal_waktu);
         facility = findViewById(R.id.facility);
+        priceTv = findViewById(R.id.price_tv);
 
         Bundle data = getIntent().getExtras();
         String key = data.getString("key");
@@ -32,9 +42,28 @@ public class BusSeatsActivity extends AppCompatActivity {
         String fasilityEt = data.getString("fasility");
         String departure = data.getString("departure");
 
+        String numericString = price.replaceAll("[^\\d]", "");
+        priceBus = Integer.parseInt(numericString);
+
         ptName.setText(pt_name);
         tanggalWaktu.setText(departure + "/" + date);
         facility.setText(fasilityEt);
+
+        for (int i = 1; i <= 24; i++) {
+            String idButton = "seat" + i;
+            int resID = getResources().getIdentifier(idButton, "id", getPackageName());
+            ImageButton button = findViewById(resID);
+            imageButtons.add(button);
+            final int finalI = i;
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    updateHasil((ImageButton) v);
+                    priceTv.setText(String.valueOf(priceFinal));
+                }
+            });
+        }
+
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,10 +82,24 @@ public class BusSeatsActivity extends AppCompatActivity {
         });
     }
 
+    private void updateHasil(ImageButton imageButton) {
+        if (imageButton.getTag() == null || !((boolean) imageButton.getTag())) {
+            priceFinal += priceBus;
+            imageButton.setBackgroundResource(R.drawable.seat_fill);
+            imageButton.setTag(true);
+        } else {
+            priceFinal -= priceBus;
+            imageButton.setBackgroundResource(R.drawable.group_15_copy_3);
+            imageButton.setTag(false);
+        }
+    }
+
+
     private void applyClickEffect(View view) {
         Animation clickAnimation = new AlphaAnimation(1F, 0.7F);
         clickAnimation.setDuration(100);
         view.startAnimation(clickAnimation);
     }
+
 
 }
